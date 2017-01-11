fb717658a7757079013c4e954a9b81f242e313c6
h4026 <h4026@ICT-421-WS-21>
2017-01-11 13:10:59 +0200

    Fine nice simple comment change.

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //h4026 demo softw made 11.1.2017
+//Toinen muutos kommenttiin jipikaijei
 namespace Hello
 {
     class Program
